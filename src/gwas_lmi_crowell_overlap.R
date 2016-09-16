76b1afd3e44df90873e2757f8423db2ecc5f7897
Tom Harrop <twharrop@gmail.com>
2016-09-16 17:29:30 +0200

    initial commit

---
+++ src/gwas_lmi_crowell_overlap.R
@@ -0,0 +1,148 @@
+#!/usr/bin/env Rscript
+
+library(data.table)
+library(ggplot2)
+
+rutils::GenerateMessage("Calculate overlaps between LMI and Crowell GWAS")
+
+# Load GWAS files
+gwas.crowell.file <- "data/gwas_crowell_genes.Rds"
+gwas.lmi.file <- "data/gwas_lmi_genes.Rds"
+
+rutils::GenerateMessage("Loading files")
+gwas.crowell <- readRDS(gwas.crowell.file)
+gwas.lmi <- readRDS(gwas.lmi.file)
+
+# deal with redundant lmi regions
+rutils::GenerateMessage("Generating regions: LMI")
+gwas.lmi <- gwas.lmi[, .(Chr, Start, End, Trait, Place, Group, bin.name)]
+gwas.lmi <- gwas.lmi[!is.na(End)]
+setkey(gwas.lmi, Chr, Start, End)
+gwas.lmi.long <- gwas.lmi[, .(
+  position = c(Start:End)
+), by = .(Chr, Start, End, Trait, Place, Group, bin.name)]
+gwas.lmi.long[, c("Start", "End") := NULL]
+
+rutils::GenerateMessage("Grouping LMI regions by Chr and position")
+lmi.regions <- gwas.lmi.long[, .(
+  lmi.trait = paste0(unique(Trait), collapse = " "),
+  lmi.subpop = paste0(unique(Group), collapse = " "),
+  lmi.year = paste0(unique(Place), collapse = " "),
+  lmi.bin = paste0(unique(bin.name), collapse = " ")
+), by = .(Chr, position)]
+
+rutils::GenerateMessage("Grouping LMI regions by lmi.bin")
+lmi.regions[, lmi.qtl :=
+              paste0("Chr", Chr, ":", min(position), "-", max(position),
+                     "|", unique(lmi.trait), "|", unique(lmi.subpop), "|",
+                     unique(lmi.year)),
+            by = .(lmi.bin)]
+
+lmi.regions[, lmi.bin := NULL]
+setnames(lmi.regions, "Chr", "chr.num")
+
+rutils::GenerateMessage("Sorting regions: LMI")
+setkey(lmi.regions, chr.num, position)
+
+# long qtls for crowell
+rutils::GenerateMessage("Generating regions: Crowell")
+crowell.regions.wide <- gwas.crowell[, .(
+  trait.collapsed = paste0(unique(TRAIT), collapse = " "),
+  subpop.collapsed = paste0(unique(SUB_POP), collapse = " ")),
+  by = .(CHR, region_start, region_end, Bin_id)]
+
+crowell.regions <- crowell.regions.wide[, .(
+  position = c(region_start:region_end),
+  trait.collapsed, subpop.collapsed, Bin_id),
+  by = .(CHR, region_start, region_end)]
+
+crowell.regions <- crowell.regions[, .(
+  chr.num = CHR, position,
+  crowell.qtl = Bin_id, crowell.trait = trait.collapsed,
+  crowell.subpop = subpop.collapsed
+  )]
+
+rutils::GenerateMessage("Sorting regions: Crowell")
+setkey(crowell.regions, chr.num, position)
+
+# merge
+rutils::GenerateMessage("Merging regions")
+merge.both <- merge(lmi.regions, crowell.regions,
+      by = c("chr.num", "position"),
+      all = TRUE)
+rutils::GenerateMessage("Sorting merged regions")
+setkey(merge.both, chr.num, position)
+
+# collapse regions
+rutils::GenerateMessage("Crowell-specific regions")
+only.crowell <- merge.both[!is.na(crowell.trait) & is.na(lmi.trait), .(
+  start = min(position), end = max(position), overlap = FALSE,
+  lmi.trait = NA, lmi.subpop = NA, lmi.year = NA, lmi.qtl = NA
+), by = .(chr.num, crowell.trait, crowell.subpop, crowell.qtl)]
+rutils::GenerateMessage("LMI-specific regions")
+only.lmi <- merge.both[is.na(crowell.trait) & !is.na(lmi.trait), .(
+  start = min(position), end = max(position), overlap = FALSE,
+  crowell.trait = NA, crowell.subpop = NA, crowell.qtl = NA
+), by = .(chr.num, lmi.trait, lmi.subpop, lmi.year, lmi.qtl)]
+rutils::GenerateMessage("Overlapping regions")
+overlap <- merge.both[!is.na(crowell.trait) & !is.na(lmi.trait), .(
+  start = min(position), end = max(position), overlap = TRUE
+), by = .(
+  chr.num, lmi.trait, lmi.subpop, lmi.year, lmi.qtl, crowell.trait,
+  crowell.subpop, crowell.qtl
+)]
+
+rutils::GenerateMessage("Combining regions")
+regions <- rbindlist(list(overlap, only.lmi, only.crowell),
+                     use.names = TRUE)
+
+rutils::GenerateMessage("Sorting regions")
+setkey(regions, chr.num, start, end)
+
+setcolorder(regions, c(
+  "chr.num", "start", "end", "overlap", "lmi.qtl", "lmi.trait", "lmi.subpop",
+  "lmi.year", "crowell.qtl", "crowell.trait", "crowell.subpop"
+))
+
+# output
+rutils::GenerateMessage("Writing")
+write.table(regions, file = "output/overlap_regions.tsv", sep = "\t",
+            quote = FALSE, na = "", row.names = FALSE)
+
+rutils::GenerateMessage("Done")
+quit(save = "no",
+     status = 0)
+
+# get unique bins
+setkey(gwas.crowell, CHR, region_start, region_end)
+gwas.crowell[, region_start := as.integer(region_start)]
+gwas.crowell[, region_end := as.integer(region_end)]
+pd.test <- gwas.crowell[, .(
+  trait.collapsed = paste0(unique(TRAIT), collapse = " "),
+  subpop.collapsed = paste0(unique(SUB_POP), collapse = " ")
+), by = .(CHR, region_start, region_end)]
+
+setkey(gwas.lmi, Chr, Start, End)
+pd.test2 <- gwas.lmi[, .(
+  trait.collapsed = paste0(unique(Trait), collapse = " "),
+  subpop.collapsed = paste0(unique(Group), collapse = " ")
+), by = .(Chr, Start, End)]
+
+
+pd.test[CHR == 1, range(region_start)]
+pd.test2[Chr == 1, range(Start)]
+
+which.chromosome <- 4
+
+ggplot() +
+  geom_rect(data = pd.test[CHR == which.chromosome],
+            mapping = aes(xmin = region_start, xmax = region_end,
+                          fill = trait.collapsed),
+            ymin = 0.25, ymax = 0.75) +
+  geom_rect(data = pd.test2[Chr == which.chromosome],
+            mapping = aes(xmin = Start, xmax = End,
+                          fill = trait.collapsed),
+            ymin = 1.25, ymax = 1.75) +
+  scale_y_continuous(limits = c(0, 2), breaks = c(0.5, 1.5),
+                     labels = c("Crowell", "LMI"))
+
---
+++ src/lmi_lmd_annotate.R
@@ -0,0 +1,62 @@
+#!/usr/bin/env Rscript
+
+library(data.table)
+
+# get tpm by expressed gene for LRT
+lrt.tpm <- data.table(readRDS("data/lmd/tpm.Rds"), keep.rownames = TRUE,
+                      key = "rn")
+setnames(lrt.tpm, "rn", "gene")
+expressed.lrt <- readRDS("data/lmd/expressedGenesAll.Rds")
+lrt.tpm <- lrt.tpm[expressed.lrt]
+
+# get LRT p-values for the LMD
+dds.lrt <- readRDS("data/lmd/ddsLrt.Rds")
+lrt.res <- data.table(data.frame(DESeq2::results(dds.lrt)),
+                      keep.rownames = TRUE, key = "rn")
+setnames(lrt.res, "rn", "gene")
+lrt.p <- unique(lrt.res[, .(gene, padj)])
+
+# combine padj and tpm
+lrt.tpm.p <- lrt.p[lrt.tpm]
+setkey(lrt.tpm.p, gene)
+
+# merge tpm with gwas results
+gwas.lmi.genes <- readRDS("data/gwas_lmi_genes.Rds")
+gwas.lmi.genes[, c("bin.name", "region.name", "gene.bin") := NULL]
+gwas.lmi.genes <- gwas.lmi.genes[!is.na(gene)]
+setkey(gwas.lmi.genes, gene)
+lmi.lmd <- lrt.tpm.p[gwas.lmi.genes]
+
+# add annotations
+annot <- lmi.lmd[, data.table(oryzr::LocToGeneName(unique(gene)),
+                              keep.rownames = TRUE, key = "rn")]
+setnames(annot, "rn", "gene")
+lmi.lmd.annot <- annot[lmi.lmd]
+sdc <- c("n1r1", "n1r3", "n1r4", "n2r1", "n2r3", "n2r4", "n3r1", "n3r2",
+         "n3r3", "n4r1", "n4r2", "n4r3")
+lmi.lmd.annot[, chuck := all(is.na(.SD)), .SDcols = sdc, by = gene]
+lmi.lmd.annot <- lmi.lmd.annot[!chuck == TRUE]
+lmi.lmd.annot[, chuck := NULL]
+setcolorder(lmi.lmd.annot, c("gene", "symbols", "names", "padj",
+                             "MsuAnnotation", "n1r1", "n1r3", "n1r4", "n2r1",
+                             "n2r3", "n2r4", "n3r1", "n3r2", "n3r3", "n4r1",
+                             "n4r2", "n4r3", "Chr", "Start", "End", "Trait",
+                             "Place", "Group", "RapID", "OgroObjective",
+                             "OgroRef"))
+
+# why are there tabs in the annotations???
+lmi.lmd.annot[, names := gsub("\t", "", names, fixed = TRUE)]
+
+# write out
+setkey(lmi.lmd.annot, Chr, Start, End)
+write.table(lmi.lmd.annot,
+            "output/lmi_lmd_annotated.tab",
+            quote = FALSE, sep = "\t",
+            na = "", row.names = FALSE)
+write.table(lmi.lmd.annot[padj < 0.05],
+            "output/lmi_lmd_annotated_sig.tab",
+            quote = FALSE, sep = "\t",
+            na = "", row.names = FALSE)
+
+
+
